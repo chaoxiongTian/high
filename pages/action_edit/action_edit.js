d6a34356d42af0fac93af89be7e09ca8e8d1e713
ciantian <ciantian@tencent.com>
2019-08-10 20:48:06 +0800

    修改活动编辑页

--- pages/action_edit/action_edit.js
+++ pages/action_edit/action_edit.js
@@ -1,4 +1,5 @@
 // pages/action_edit/action_edit.js
+var util = require('../utils/util.js');
 Page({
 
   /**
@@ -12,7 +13,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 调用函数时，传入new Date()参数，返回值是日期和时间
+    var time = util.formatTime(new Date());
+    // 再通过setData更改Page()里面的data，动态更新页面的数据
+    this.setData({
+      time: time
+    });
   },
 
   /**
--- pages/action_edit/action_edit_time_select/action_edit_time_select.js
+++ pages/action_edit/action_edit_time_select/action_edit_time_select.js
@@ -1,18 +1,25 @@
+
 const date = new Date()
 const years = []
 const months = []
 const days = []
+const hours = []
+const minutes = []
 
 for (let i = date.getFullYear(); i <= date.getFullYear() + 10; i++) {
   years.push(i)
 }
-
 for (let i = 1; i <= 12; i++) {
   months.push(i)
 }
-
 for (let i = 1; i <= 31; i++) {
   days.push(i)
+}
+for (let i = 1; i <= 24; i++) {
+  hours.push(i)
+}
+for (let i = 0; i <= 59; i++) {
+  minutes.push(i)
 }
 
 Page({
@@ -27,20 +34,23 @@ Page({
     years,
     year: date.getFullYear(),
     months,
-    month: 2,
+    month: 1,
     days,
-    day: 2,
+    day: 1,
     value: [9999, 1, 1],
-    isDaytime: true,
+    hours,
+    hours:24,
+    minutes,
+    minutes:59,
   },
-
   bindChange(e) {
     const val = e.detail.value
     this.setData({
       year: this.data.years[val[0]],
-      month: this.data.months[val[1]],
-      day: this.data.days[val[2]],
-      isDaytime: !val[3]
+      month: this.data.months[val[0]],
+      day: this.data.days[val[0]],
+      hours: this.data.days[val[0]],
+      minutes: this.data.minutes[val[0]],
     })
   }
 }) 
