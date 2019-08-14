4af4ef7db1efb958b6557ee52a03f953e1b9c139
Chonor <caihonor@gmail.com>
2019-08-15 00:51:09 +0800

    增加所有好友

--- pages/index/index.js
+++ pages/index/index.js
@@ -30,6 +30,7 @@ Page({
     friends:[],
     friendsCount:0,
     friendsZindex:10,
+    friendsAvator:[],
   },
   //事件处理函数
   bindViewTap: function() {
@@ -61,6 +62,17 @@ Page({
         })
       }
     }
+    setTimeout(function () {
+      app.getLocal(that);
+      that.getFriendInfo();
+      that.setMarkers();
+      if (!that.data.hasMarker) {
+        that.getUserInfo();
+      }
+      if (that.data.hasAvatar && that.data.hasMarker) {
+        that.updatePos();
+      }
+    }, 2000)
   },
   onFriendsClose(){
     this.setData({
@@ -101,7 +113,6 @@ Page({
     console.log("actionBtn");
   },
   tapFriend(){
-    this.getFriendInfo();
     this.setData({
       friendsZindex: 1000,
     })
@@ -181,7 +192,7 @@ Page({
   },
   setMarkers() {
 
-    var markers = [{
+    var markers = {
       iconPath: this.data.avatarUrl,
       latitude: this.data.latitude,
       longitude: this.data.longitude,
@@ -197,10 +208,10 @@ Page({
         padding: 5, 
         display: 'ALWAYS' 
         }
-    }]
+    }
     this.setData({
       hasMarker: true,
-      markers: markers
+      'markers[0]': markers
     })
     //console.log('marker:',markers);
     //console.log(this.data.hasMarker);
@@ -217,7 +228,7 @@ Page({
       if (that.data.hasAvatar&&that.data.hasMarker){
         that.updatePos();
       }
-    }, 2000)
+    }, 10000)
   },
 
   onClickAction: function (e) {
@@ -241,6 +252,9 @@ Page({
 
   updateFriendInfo:function(id) {
     var that = this;
+    if (id == app.globalData.userOpenId) {
+      return;
+    }
     wx.request({
       url: 'http://149.28.31.199/update_friend.php',
       data: {
@@ -282,6 +296,12 @@ Page({
             name: res.data.nickname[i],
             openId: res.data.frientId[i],
             avator: res.data.friendAvator[i],
+            cachePath: '',
+            longitude:'',
+            latitude:'',
+            hasAction:false,
+            actionId:'',
+            actionName:'',
           }
           friends[i]=oneFriend;
         }
@@ -293,6 +313,7 @@ Page({
           })
           app.globalData.friends = friends;
           app.globalData.friendsCount = res.data.count;
+          that.getAllFriends();
         }
       },
       fail: function () {
@@ -331,4 +352,84 @@ Page({
       }
     })
   },
+
+  getAllFriends() {
+    console.log("getAllFriends----------------");
+    var that = this;
+    for (let i=0; i<that.data.friendsCount; i++){
+      wx.request({
+        url: 'http://149.28.31.199/update_position.php',
+        data: {
+          wxid: that.data.friends[i].openId,
+          longitude: 0,
+          latitude: 0,
+          avator: '',
+          nickname: '',
+        },
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+        },
+        dataType: "json",
+        method: 'POST',
+        success: function (res) {
+          if (res.statusCode == 200) {
+            console.log("update_position_friends:", res.data)
+            var t1 = 'friends[' + i + '].longitude';
+            var t2 = 'friends[' + i + '].latitude';
+            var t3 = 'friends[' + i + '].hasAction';
+            var t4 = 'friends[' + i + '].actionName';
+            var t5 = 'friends[' + i + '].actionId';
+            that.setData({
+              [t1]: res.data.longitude,
+              [t2]: res.data.latitude,
+              [t3]: res.data.hasAction,
+              [t4]: res.data.actionName[0],
+              [t5]: res.data.actionId[0],
+            })
+          }
+        },
+        fail: function () {
+          console.log("update_position fail")
+        }
+      })
+      that.getAvatarImage(that.data.friends[i].avator).then(function (cachePath) {
+        console.log('xxxxxx  ', cachePath);
+        var t1 = 'friends[' + i + '].cachePath';
+        that.setData({
+          [t1]:cachePath,
+        })
+      })
+    }
+    setTimeout(function () {
+      that.setFriendsMarker();
+    }, 5000)
+  },
+  setFriendsMarker(){
+    console.log('setFriends:',this.data.friends);
+    for (var i=0; i<this.data.friendsCount; i++){
+      var markers = {
+        iconPath: this.data.friends[i].cachePath,
+        latitude: this.data.friends[i].latitude,
+        longitude: this.data.friends[i].longitude,
+        height: 50,
+        width: 50,
+        id: i+1,
+        callout: {
+          content: this.data.friends[i].hasAction ? this.data.friends[i].actionName : this.data.friends[i].name,
+          color: "#FFFFFF",
+          fontSize: 18,
+          borderRadius: 20,
+          bgColor: "#FF4081",
+          padding: 5,
+          display: 'ALWAYS'
+        }
+      }
+      console.log('setFriendsMark:',i,markers);
+      var t1 = 'markers[' + [i+1] + ']';
+      this.setData({
+        [t1]: markers
+      })
+    }
+    console.log('setFriendsMark:', this.data.markers);
+  }
 })
